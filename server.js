5f84a542197d9a5231f5dce425a18b7def8837a7
gigimerry <69761091+gigimerry@users.noreply.github.com>
2023-05-09 15:14:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -62,9 +62,45 @@ const app = express()
 // Set a port for the server to listen on
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
-//  
+//
+
+import { rps, rpsls } from "./lib/rpsls.js"
+// Parsing requests with url request
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK').end();
+});
 
 
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
+})
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
+})
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
+})
 
 
 // Create and update access log
