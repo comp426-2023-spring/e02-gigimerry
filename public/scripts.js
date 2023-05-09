9625e08d9a1f695c66283bc47af59f36e90901ee
gigimerry <69761091+gigimerry@users.noreply.github.com>
2023-05-09 18:40:18 -0400

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,13 @@
-
-var hasOpponent = false;
+var hasOpp = false;
 var rpsVersion = true;
-
 
 async function play() {
     var winner = document.getElementById("winner");
     var rand_move = document.getElementById("rand_move")
     var player = document.getElementById("playerMove");
-    var opponent = document.getElementById("opponentMove");
-    var opponentGame = document.getElementById("opponentGame")
-    if (rpsVersion && hasOpponent) {
+    var opp = document.getElementById("opponentMove");
+    var oppGame = document.getElementById("opponentGame")
+    if (rpsVersion && hasOpp) {
         var move;
         var radios = document.getElementsByName("move");
         for (var i = 0, length = radios.length; i < length; i++) {
@@ -21,19 +19,19 @@ async function play() {
         const response = await fetch(`/app/rps/play/${move}`);
         const data = await response.json();
         player.innerHTML = `Player move: ${data.player}`;
-        opponent.innerHTML = `Opponent move: ${data.opponent}`;
+        opp.innerHTML = `Opponent move: ${data.opp}`;
         winner.innerHTML = `Result: ${data.result}`;
-        opponentGame.className = "active";
+        oppGame.className = "active";
         rand_move.className = "inactive";
     }
     else if (rpsVersion) {
         const response = await fetch(`/app/rps/play`);
         const data = await response.json();
         rand_move.innerHTML = `Move: ${data.player}`;
-        opponentGame.className = "inactive"
+        oppGame.className = "inactive"
         rand_move.className = "active";
     }
-    else if (hasOpponent) {
+    else if (hasOpp) {
         var move;
         var radios = document.getElementsByName("move");
         for (var i = 0, length = radios.length; i < length; i++) {
@@ -45,16 +43,16 @@ async function play() {
         const response = await fetch(`/app/rpsls/play/${move}`);
         const data = await response.json();
         player.innerHTML = `Player move: ${data.player}`;
-        opponent.innerHTML = `Opponent move: ${data.opponent}`;
+        opp.innerHTML = `Opponent move: ${data.opp}`;
         winner.innerHTML = `Result: ${data.result}`;
-        opponentGame.className = "active"
+        oppGame.className = "active"
         rand_move.className = "inactive";
     }
     else {
         const response = await fetch(`/app/rpsls/play`);
         const data = await response.json();
         rand_move.innerHTML = `Move: ${data.player}`;
-        opponentGame.className = "inactive"
+        oppGame.className = "inactive"
         rand_move.className = "active"
     }
     var modal = document.getElementById("modal");
@@ -67,36 +65,36 @@ async function reset() {
 
 async function rpslsSelected() {
     rpsVersion = false;
-    opponentChange();
+    oppChange();
     console.log(rpsVersion);
 }
 
 async function rpsSelected() {
     rpsVersion = true;
-    opponentChange();
+    oppChange();
     console.log(rpsVersion);
 }
 
-async function opponentChange() {
-    const opponentCheck = document.querySelector('#opponent');
+async function oppChange() {
+    const oppCheck = document.querySelector('#opponent');
     const rpsSect = document.getElementById("rps");
     const rpslsSect = document.getElementById("rpsls")
-    console.log(opponentCheck.checked)
+    console.log(oppCheck.checked)
     rpsSect.className = "active";
-    if (opponentCheck.checked && rpsVersion) {
+    if (oppCheck.checked && rpsVersion) {
         rpsSect.className = "active";
         rpslsSect.className = "inactive";
-        hasOpponent = true;
+        hasOpp = true;
     }
-    else if (opponentCheck.checked && !rpsVersion) {
+    else if (oppCheck.checked && !rpsVersion) {
         rpsSect.className = "active";
         rpslsSect.className = "active";
-        hasOpponent = true;
+        hasOpp = true;
     }
     else {
         rpsSect.className = "inactive";
         rpslsSect.className = "inactive";
-        hasOpponent = false;
+        hasOpp = false;
     }
 
 }
